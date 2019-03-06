a67fd1e13231938b31ca3bdf1dff56e2afda4c48
Christian Chavarro <chavarro.christian@gmail.com>
2019-03-05 22:49:10 -0600

    Wired up student information component.

--- components/Student/AccountPage/components/StudentInformation/index.js
+++ components/Student/AccountPage/components/StudentInformation/index.js
@@ -1,34 +1,60 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const StudentInformation = () => (
+import Dropdown from '../../../../FormComponents/Dropdown';
+
+import getValueFromState from '../../../../utils/getValueFromState';
+import genderOptions from '../../../../utils/genderOptions';
+
+const StudentInformation = ({ state: { firstName, lastName, gender }, handleDetailsChange }) => (
   <div className="card-block">
     <h2>Student Information</h2>
     <div className="card">
       <div className="card-content">
         <div className="row mb-0">
           <div className="input-field col s12 focus-blue">
-            <input type="text" id="first_name" value="Arnold" />
-            <label className="label" htmlFor="first_name">First Name</label>
+            <input
+              type="text"
+              id="first_name"
+              name="firstName"
+              value={firstName}
+              onChange={(event) => handleDetailsChange(event, 'firstName', 'studentInformation')}
+            />
+            <label className={firstName.length ? 'label active' : 'label'} htmlFor="first_name">First Name</label>
           </div>
         </div>
         <div className="row mb-0">
           <div className="input-field col s12 focus-blue">
-            <input type="text" id="last_name" value="Studently" />
-            <label className="label" htmlFor="last_name">Last Name</label>
+            <input
+              type="text"
+              id="last_name"
+              name="lastName"
+              value={lastName}
+              onChange={(event) => handleDetailsChange(event, 'lastName', 'studentInformation')}
+            />
+            <label className={lastName.length ? 'label active' : 'label'} htmlFor="last_name">Last Name</label>
           </div>
         </div>
         <div className="row mb-0">
           <div className="input-field col s6 l5 focus-blue">
-            <select id="state_edit">
-              <option>Male</option>
-              <option>Female</option>
-            </select>
-            <label className="label" htmlFor="state_edit">Gender</label>
+            <Dropdown
+              value={getValueFromState(gender, genderOptions)}
+              onChange={(event) => handleDetailsChange(event, 'gender', 'studentInformation')}
+              options={genderOptions}
+              label="Gender"
+              stateKey="gender"
+              dropdownKey="gender"
+            />
           </div>
         </div>
       </div>
     </div>
   </div>
 );
+
+StudentInformation.propTypes = {
+  state: PropTypes.object.isRequired,
+  handleDetailsChange: PropTypes.func.isRequired,
+};
 
 export default StudentInformation;
