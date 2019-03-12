b38186045f2fb355435240f3405b0966d3c7db74
Christian Chavarro <chavarro.christian@gmail.com>
2019-03-11 22:51:20 -0500

    Initial commit for location detail account summary page, separated navbar into separate component.

---
+++ components/Location/DetailAccountPage/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class DetailAccountPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+  render() {
+    // TODO: Update navbar info with actual sample location data
+    return (
+      <React.Fragment></React.Fragment>
+    );
+  }
+}
+
+export default DetailAccountPage;
