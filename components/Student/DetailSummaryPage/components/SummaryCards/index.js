5867f1694d533f86d642985a84eeeeb4064fca8d
Christian Chavarro <chavarro.christian@gmail.com>
2019-03-29 21:12:04 -0500

    Wired up interactive strengths/weaknesses, summary cards, and test scores component with associated actions to conditionally render user info.

--- components/Student/DetailSummaryPage/components/SummaryCards/index.js
+++ components/Student/DetailSummaryPage/components/SummaryCards/index.js
@@ -1,15 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// eslint-disable-next-line no-unused-vars
-const SummaryCards = ({ state }) => (
+const formatTimeLoggedIn = (totalMinutes) => {
+  const hours = Math.floor(totalMinutes / 60) === 0 ? 0 : Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60 === 0 ? 0 : totalMinutes % 60;
+  return (
+    <React.Fragment>
+      { hours !== 0 && (
+        <React.Fragment>
+          <b className="text-large">{hours}</b>
+          <span className="text-small">hrs </span>
+        </React.Fragment>
+      )}
+      { minutes !== 0 && (
+        <React.Fragment>
+          <b className="text-large">{minutes}</b>
+          <span className="text-small">min</span>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
+}
+
+const formatVideoWatched = (totalMinutes) => {
+  const hours = Math.floor(totalMinutes / 60) === 0 ? 0 : Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60 === 0 ? 0 : totalMinutes % 60;
+  return (
+    <React.Fragment>
+      { hours !== 0 && (
+        <React.Fragment>
+          <b className="text-large">{hours}</b>
+          <span className="text-small">hrs </span>
+        </React.Fragment>
+      )}
+      { minutes !== 0 && (
+        <React.Fragment>
+          <b className="text-large">{minutes}</b>
+          <span className="text-small">min</span>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
+}
+
+const SummaryCards = ({ state: { questionsAnswered, videoWatched, notesTaken, totalTimeLoggedIn, lastLogIn, loginTimeCode, onTimePercentage} }) => (
   <div className="col s12 l5 card-width-366">
     <ul className="alerts-list">
       <li className="card card-alert">
         <span className="icon-box purple darken-3"><i className="icon-pencil2"></i></span>
         <div className="alert-text purple-text text-darken-3">
           <p><b>Questions Answered</b></p>
-          <div className="value"><b className="text-large">734</b></div>
+          <div className="value"><b className="text-large">{questionsAnswered}</b></div>
         </div>
       </li>
       <li className="card card-alert alert-blue-dark">
@@ -17,7 +58,7 @@ const SummaryCards = ({ state }) => (
         <div className="alert-text cyan-text text-darken-2">
           <p><b>Video Watched</b></p>
           <div className="value">
-            <b className="text-large">4</b><span className="text-small">hrs</span> <b className="text-large">26</b><span className="text-small">min</span>
+            {formatVideoWatched(videoWatched)}
           </div>
         </div>
       </li>
@@ -25,7 +66,7 @@ const SummaryCards = ({ state }) => (
         <span className="icon-box pink darken-2"><i className="icon-note"></i></span>
         <div className="alert-text pink-text text-darken-2">
           <p><b>Notes Taken</b></p>
-          <div className="value"><b className="text-large">59</b></div>
+          <div className="value"><b className="text-large">{notesTaken}</b></div>
         </div>
       </li>
       <li className="card card-alert">
@@ -33,7 +74,7 @@ const SummaryCards = ({ state }) => (
         <div className="alert-text green-text text-darken-3">
           <p><b>Total Time Logged In</b></p>
           <div className="value">
-            <b className="text-large">8</b><span className="text-small">hrs</span> <b className="text-large">14</b><span className="text-small">min</span>
+            {formatTimeLoggedIn(totalTimeLoggedIn)}
           </div>
         </div>
       </li>
@@ -42,7 +83,7 @@ const SummaryCards = ({ state }) => (
         <div className="alert-text orange-text text-darken-2">
           <p><b>Last Log In</b></p>
           <div className="value">
-            <span className="text-large">3:48</span> <span className="text-small">pm</span>
+            <span className="text-large">{lastLogIn}</span> <span className="text-small">{loginTimeCode}</span>
           </div>
         </div>
       </li>
@@ -50,7 +91,7 @@ const SummaryCards = ({ state }) => (
         <span className="icon-box red"><i className="icon-bell"></i></span>
         <div className="alert-text red-text">
           <p><b>On-Time Percentage</b></p>
-          <div className="value"><b className="text-large">73%</b></div>
+          <div className="value"><b className="text-large">{onTimePercentage}%</b></div>
         </div>
       </li>
     </ul>
