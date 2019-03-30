3b9e41ba7c92d041928536c8fc6617a6195d8286
Christian Chavarro <chavarro.christian@gmail.com>
2019-03-29 21:10:36 -0500

    Added missing sample data for student detail summary page, passed state into container and page's respective components.

--- components/Student/DetailSummaryPage/index.js
+++ components/Student/DetailSummaryPage/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import CourseProgress from './components/CourseProgress';
 import OverdueWork from './components/OverdueWork';
 import SummaryCards from './components/SummaryCards';
@@ -8,24 +10,29 @@ import StrengthsAndWeaknesses from './components/StrengthsAndWeaknesses';
 // eslint-disable-next-line react/prefer-stateless-function
 class DetailSummaryPage extends React.Component {
   render() {
+    const { user: { courseProgress, overdueWork, summary, testScores, strengthsAndWeaknesses } } = this.props;
     return (
       <div className="content-section">
         <div className="container">
           <div className="cards-section">
             <div className="row d-flex justify-content-center mb-0">
-              <CourseProgress />
-              <OverdueWork />
+              <CourseProgress state={courseProgress} />
+              <OverdueWork state={overdueWork} />
             </div>
             <div className="row d-flex justify-content-center mb-0">
-              <SummaryCards />
-              <TestScores />
+              <SummaryCards state={summary} />
+              <TestScores state={testScores} />
             </div>
           </div>
-          <StrengthsAndWeaknesses />
+          <StrengthsAndWeaknesses state={strengthsAndWeaknesses} />
         </div>
       </div>
     );
   }
 }
+
+DetailSummaryPage.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default DetailSummaryPage;
