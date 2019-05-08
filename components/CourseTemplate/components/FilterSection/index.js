ee6c94b501f58b19b50252d02639d88cbe5840ea
Christian Chavarro <chavarro.christian@gmail.com>
2019-05-07 22:34:37 -0500

    Basic filter and sort state setup in course template FilterSection component

--- components/CourseTemplate/components/FilterSection/index.js
+++ components/CourseTemplate/components/FilterSection/index.js
@@ -1,20 +1,44 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+
+import Dropdown from '../../../FormComponents/Dropdown';
+import getValueFromState from '../../../utils/getValueFromState';
+
+import sortOptions from '../../utils/sortOptions';
 
 class FilterSection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      test: true,
+      open: true,
+      title: '',
+      sort: '',
     };
   }
 
+  onToggleShowFilters = () => this.setState(({ open }) => ({ open: !open }))
+  onClearFilters = () => this.setState({ title: '', sort: '' }, this.props.onClearFilters)
+
+  // Strips the entered name of any spaces and capitalizing and fires off the TemplateList event that filters the instructors
+  submitNameFilter = () => {
+    const { onSetFilteredState, onUnsetFilteredState } = this.props;
+    const { title } = this.state;
+    if (title === '') {
+      onUnsetFilteredState();
+    }
+    const transformedTitle = title.replace(/\s/g, "").toLowerCase();
+    onSetFilteredState(transformedTitle);
+  }
+
   render() {
+    const { sort, open } = this.state;
+    const { onClearFilters } = this.props;
     return (
       <div className="filter-form-holder">
-        <ul className="collapsible expandable">
+        <ul className="collapsible expandable" style={{ minHeight: '0' }}>
           <li>
-            <div className="collapsible-body">
+            <div className="collapsible-body" style={open ? { display: 'block' } : { display: 'none' }}>
               <div className="filter-form_checkbox-list-holder justify-center">
                 <ul className="filter-form_checkbox-list">
                   <li>
@@ -59,12 +83,15 @@ class FilterSection extends React.Component {
               <div className="col s12 l4">
                 <div className="row mb-0">
                   <div className="col s12 xl7">
-                    <div className="input-field">
-                      <select id="sort_courses">
-                        <option>Number of Sessions</option>
-                        <option>Number of Sessions</option>
-                      </select>
-                      <label className="label" htmlFor="sort_courses">Sort By</label>
+                    <div className="input-field" style={{ marginBottom: '0', marginTop: '0' }}>
+                      <Dropdown
+                        value={getValueFromState(sort, sortOptions)}
+                        onChange={(event) => this.handleFilterChange(event, 'sort')}
+                        options={sortOptions}
+                        label="Sort By"
+                        stateKey="topic"
+                        dropdownKey="topic"
+                      />
                     </div>
                   </div>
                 </div>
@@ -72,9 +99,11 @@ class FilterSection extends React.Component {
               <div className="col s12 l4 hide-on-med-and-down show-on-large">&nbsp;</div>
               <div className="col s12 l4">
                 <div className="option-filters">
-                  <div className="option-item clear"><a href="#">Clear Filters</a></div>
+                  <div className="option-item clear"><a href="#" onClick={onClearFilters}>Clear Filters</a></div>
                   <div className="option-item">
-                    <span className="collapsible-header"><span className="hide-text">Hide</span> <span className="open-text">Open</span> Filters</span>
+                    <span className="collapsible-header" onClick={this.onToggleShowFilters}>
+                      <span className="open-text">{open ? 'Hide Filters' : 'Open Filters'}</span>
+                    </span>
                   </div>
                 </div>
               </div>
@@ -84,6 +113,12 @@ class FilterSection extends React.Component {
       </div>
     );
   }
+}
+
+FilterSection.propTypes = {
+  onClearFilters: PropTypes.func.isRequired,
+  onSetFilteredState: PropTypes.func.isRequired,
+  onUnsetFilteredState: PropTypes.func.isRequired,
 }
 
 export default FilterSection;
