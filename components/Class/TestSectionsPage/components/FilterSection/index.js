d8701d64ec52a2212cc2b9848eeb02e827e17798
Christian Chavarro <chavarro.christian@gmail.com>
2019-05-30 18:33:36 -0500

    Minor state change.

--- components/Class/TestSectionsPage/components/FilterSection/index.js
+++ components/Class/TestSectionsPage/components/FilterSection/index.js
@@ -10,7 +10,7 @@ class FilterSection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: true,
+      open: false,
       testVersionFilter: '',
     };
   }
