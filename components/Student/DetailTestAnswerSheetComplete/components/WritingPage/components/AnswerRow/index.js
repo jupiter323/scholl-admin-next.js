7277106842fd75db6919f4101fbe8f3beb8ab6b1
kjojones <43149867+kfroerer@users.noreply.github.com>
2019-06-07 15:00:50 -0500

    bubble sheet creation, answer row changes

--- components/Student/DetailTestAnswerSheetComplete/components/WritingPage/components/AnswerRow/index.js
+++ components/Student/DetailTestAnswerSheetComplete/components/WritingPage/components/AnswerRow/index.js
@@ -1,94 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import QuestionModal from '../QuestionModal';
+import BubbleGroup from '../Bubble';
 
 class AnswerRow extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       open: false,
-     }
+    }
   };
-  onOpenQuestionModal = () => this.setState({open: true})
-  onCloseQuestionModal = () => this.setState({open: false});
 
+  onOpenQuestionModal = () => this.setState({ open: true })
+  onCloseQuestionModal = () => this.setState({ open: false });
 
-  mapLetterBubbles = ({index}) => {
-    const letters = ["A", "B", "C", "D"];
+  mapNumberBubbles = ({ index }) => {
     const { sampleAnswers } = this.props;
-    const { studentChoice, answer } = sampleAnswers[index];
-    return letters.map(letter => {
-      if (letter === answer && letter === studentChoice){
-        return (
-          <li key={letter}>
-          <span className="badge-circle badge-circle-bordered"  style={{color: '#fff', borderColor: '#32955c', backgroundColor: '#3eb777'}}>{letter}</span>
-        </li>
-        )
-      }
-      if (letter === answer && letter !== studentChoice){
-        return (
-          <li key={letter}>
-            <span className="badge-circle badge-circle-bordered" style={{color: '#32955c', borderColor: '#32955c', backgrounColor: '#fff'}}>{letter}</span>
-          </li>
-        )
-      }
-      if (letter !== answer && letter === studentChoice){
-        return (
-          <li key={letter}>
-            <span className="badge-circle badge-circle-bordered" style={{color: '#fff', borderColor: '#ad1e3e', backgroundColor: '#db1d41'}}>{letter}</span>
-          </li>
-        )
-      }
+    const { answer, studentChoice } = sampleAnswers[index];
+    if (answer === studentChoice) {
       return (
-          <li key={letter}>
-            <span className="badge-circle badge-circle-bordered">{letter}</span>
-          </li>
-        )
-      }
+        <li key={answer}>
+          <span className="badge badge-rounded badge-rounded-bordered" style={{ color: '#fff', borderColor: '#32955c', backgroundColor: '#3eb777' }}>{answer}</span>
+        </li>
+      )
+    }
+    return (
+      <React.Fragment>
+        <li key={studentChoice}>
+          <span className="badge badge-rounded badge-rounded-bordered" style={{ color: '#fff', borderColor: '#ad1e3e', backgroundColor: '#db1d41' }}>{studentChoice}</span>
+        </li>
+        <li key={answer} >
+          <span className="badge badge-rounded badge-rounded-bordered" style={{ color: '#32955c', borderColor: '#32955c', backgroundColor: '#fff' }}>{answer}</span>
+        </li>
+      </React.Fragment>
     )
   };
 
-  mapNumberBubbles = ({ index} ) => {
-    const { sampleAnswers } = this.props;
-    const { answer, studentChoice }= sampleAnswers[index];
-          if ( answer === studentChoice) {
-        return (
-          <li key={answer}>
-            <span className="badge badge-rounded badge-rounded-bordered" style={{color: '#fff', borderColor: '#32955c', backgroundColor: '#3eb777'}}>{answer}</span>
-          </li>
-        )
-      }
-      return (
-          <React.Fragment>
-            <li key={studentChoice}>
-              <span className="badge badge-rounded badge-rounded-bordered"  style={{color: '#fff', borderColor: '#ad1e3e', backgroundColor: '#db1d41'}}>{studentChoice}</span>
-            </li>
-            <li key={answer} >
-              <span className="badge badge-rounded badge-rounded-bordered" style={{color: '#32955c', borderColor: '#32955c', backgroundColor: '#fff'}}>{answer}</span>
-            </li>
-          </React.Fragment>
-        )
-      };
-
-      render() {
-        const { index, question } = this.props;
-        const { open } = this.state;
-        return (
-          <React.Fragment>
-          <QuestionModal open={open} onOpenQuestionModal={this.onOpenQuestionModal} onCloseQuestionModal={this.onCloseQuestionModal} question={question}/>
+ render() {
+    const { question, index } = this.props;
+    const { open } = this.state;
+    return (
+      <React.Fragment>
+        <QuestionModal open={open} onOpenQuestionModal={this.onOpenQuestionModal} onCloseQuestionModal={this.onCloseQuestionModal} question={question} />
         <li className="answers-list-holder" key={question.problem}>
-          <div className="answer-row row mb-0">
+          <div className="answer-row row mb-0" >
             <div className="col col-120">
-              <ul className="answer-list" >
+              <ul className="answer-list">
                 <Choose>
                   <When condition={!question.numeric}>
-                  {this.mapLetterBubbles({index})}
+                    <BubbleGroup
+                      id={question.id}
+                      question={question}
+                    />
+                    {/* <Choose>
+                      <When condition={studentChoice === ""}>
+                          {this.renderAnsweredBubbles(question.id)}
+                        </When>
+                        <Otherwise>
+                          {this.mapEmptyBubbles(question.id)}
+                        </Otherwise>
+                      </Choose> */}
                   </When>
                   <Otherwise>
-                  {this.mapNumberBubbles({index})}
+                    {this.mapNumberBubbles({ index })}
                   </Otherwise>
                 </Choose>
-               </ul>
+              </ul>
             </div>
             <div className="col col-30">
               <span className="status-info">{question.status}</span>
@@ -97,16 +74,16 @@ class AnswerRow extends React.Component {
               <span className="play-progress" data-video-id="video001">
                 <span className="play"></span>
                 <svg viewBox="0 0 50 50" width="50" height="50" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                  <circle className="circle-static" cx={25.8} cy={24.3} r={22.8} fill='transparent' strokeWidth={3}/>
-                  <circle className="circle-progress" cx={25.8} cy={24.3} r={22.8} fill='transparent' strokeWidth={3}/>
+                  <circle className="circle-static" cx={25.8} cy={24.3} r={22.8} fill='transparent' strokeWidth={3} />
+                  <circle className="circle-progress" cx={25.8} cy={24.3} r={22.8} fill='transparent' strokeWidth={3} />
                 </svg>
               </span>
             </div>
             <div className="col col-auto">
               <If condition={question.flagged}>
-                  <span className="status-answer" style={{color: '#c0272d'}}>
-                    <i className="icon-flag"></i><b className="status-text">Review</b>
-                  </span>
+                <span className="status-answer" style={{ color: '#c0272d' }}>
+                  <i className="icon-flag"></i><b className="status-text">Review</b>
+                </span>
               </If>
             </div>
             <div className="dropdown-block col col-35">
@@ -133,4 +110,5 @@ AnswerRow.propTypes = {
 }
 
 export default AnswerRow;
+
 
