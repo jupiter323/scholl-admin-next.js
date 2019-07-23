68ffc5d21c89cce565a158c12906b7622b9b2389
kjojones <43149867+kfroerer@users.noreply.github.com>
2019-07-23 09:25:47 -0500

    merged master in

--- .next/server/static/development/pages/_app.js
+++ .next/server/static/development/pages/_app.js
@@ -109,10 +109,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _babel_runtime_corejs2_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/inherits */ "./node_modules/@babel/runtime-corejs2/helpers/esm/inherits.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! react */ "react");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_5__);
-/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
-/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_6__);
-/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! jquery */ "jquery");
-/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_7__);
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! prop-types */ "prop-types");
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_6__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_7__);
+/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! jquery */ "jquery");
+/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_8___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_8__);
 
 
 
@@ -126,9 +128,10 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+
 if (typeof window !== 'undefined') {
-  window.$ = jquery__WEBPACK_IMPORTED_MODULE_7___default.a;
-  window.jQuery = jquery__WEBPACK_IMPORTED_MODULE_7___default.a; // eslint-disable-next-line global-require
+  window.$ = jquery__WEBPACK_IMPORTED_MODULE_8___default.a;
+  window.jQuery = jquery__WEBPACK_IMPORTED_MODULE_8___default.a; // eslint-disable-next-line global-require
 
   __webpack_require__(/*! materialize-css */ "materialize-css");
 }
@@ -190,7 +193,7 @@ function (_Component) {
   Object(_babel_runtime_corejs2_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_1__["default"])(SideNav, [{
     key: "componentDidMount",
     value: function componentDidMount() {
-      jquery__WEBPACK_IMPORTED_MODULE_7___default()('.sidenav').sidenav();
+      jquery__WEBPACK_IMPORTED_MODULE_8___default()('.sidenav').sidenav();
     }
   }, {
     key: "handleClick",
@@ -204,6 +207,9 @@ function (_Component) {
     value: function render() {
       var _this2 = this;
 
+      var _this$props$user = this.props.user,
+          firstName = _this$props$user.firstName,
+          lastName = _this$props$user.lastName;
       return react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("aside", {
         id: "slide-out",
         className: "sidenav"
@@ -221,27 +227,33 @@ function (_Component) {
             return _this2.handleClick(menuItem.key);
           },
           key: menuItem.key
-        }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_6___default.a, {
+        }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_7___default.a, {
           href: "/".concat(menuItem.page)
         }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("a", null, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("i", {
           className: menuIconMap[menuItem.key]
         }), menuItem.key.charAt(0).toUpperCase() + menuItem.key.slice(1))));
-      })), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("a", {
-        href: "#",
+      })), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("div", {
         className: "log-block white-text"
       }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("div", {
         className: "text"
+      }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("a", {
+        href: "#"
       }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("div", {
-        className: "block"
-      }, "Log Out"), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("div", {
-        className: "block"
-      }, "Joe Smith")))));
+        className: "block white-text"
+      }, "Log Out")), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_7___default.a, {
+        href: "/account"
+      }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("a", null, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("div", {
+        className: "block white-text"
+      }, firstName, " ", lastName)))))));
     }
   }]);
 
   return SideNav;
 }(react__WEBPACK_IMPORTED_MODULE_5__["Component"]);
 
+SideNav.propTypes = {
+  user: prop_types__WEBPACK_IMPORTED_MODULE_6___default.a.object.isRequired
+};
 /* harmony default export */ __webpack_exports__["default"] = (SideNav);
 
 /***/ }),
@@ -287,7 +299,7 @@ var Meta = function Meta() {
   }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("link", {
     rel: "stylesheet",
     href: "/static/css/main.css"
-  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("title", null, "Clean Choice Test Prep"));
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("title", null, "Clear Choice Test Prep"));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (Meta);
@@ -316,6 +328,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(styled_components__WEBPACK_IMPORTED_MODULE_7__);
 /* harmony import */ var _Meta__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./Meta */ "./components/Meta.js");
 /* harmony import */ var _Core_SideNav__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./Core/SideNav */ "./components/Core/SideNav.js");
+/* harmony import */ var _components_utils_sampleInstructors__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ../components/utils/sampleInstructors */ "./components/utils/sampleInstructors.js");
+
 
 
 
@@ -360,7 +374,9 @@ function (_Component) {
         theme: theme
       }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(StyledPage, null, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(_Meta__WEBPACK_IMPORTED_MODULE_8__["default"], null), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("div", {
         className: "wrapper"
-      }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(_Core_SideNav__WEBPACK_IMPORTED_MODULE_9__["default"], null), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(Inner, null, this.props.children)), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("script", {
+      }, react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(_Core_SideNav__WEBPACK_IMPORTED_MODULE_9__["default"], {
+        user: _components_utils_sampleInstructors__WEBPACK_IMPORTED_MODULE_10__["default"][0]
+      }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(Inner, null, this.props.children)), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("script", {
         src: "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"
       }), react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("link", {
         rel: "stylesheet",
@@ -382,6 +398,79 @@ Page.propTypes = {
   children: prop_types__WEBPACK_IMPORTED_MODULE_6___default.a.any
 };
 /* harmony default export */ __webpack_exports__["default"] = (Page);
+
+/***/ }),
+
+/***/ "./components/utils/sampleInstructors.js":
+/*!***********************************************!*\
+  !*** ./components/utils/sampleInstructors.js ***!
+  \***********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ([{
+  firstName: 'Bob',
+  lastName: 'Smith',
+  email: 'bob@smith.com',
+  addressLine1: '4455 Main St',
+  city: "Portland",
+  state: "OR",
+  zipCode: "98758",
+  timeZone: "Pacific",
+  avatarURL: "https://ichef.bbci.co.uk/news/320/media/images/82399000/jpg/_82399978_75952740.jpg"
+}, {
+  firstName: 'Jeff',
+  lastName: 'Bezos',
+  email: 'jeff-bezos@email.com',
+  addressLine1: '1234 Test Lane',
+  city: 'Austin',
+  state: 'TX',
+  zipCode: '78757',
+  timeZone: "Central",
+  avatarURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUGJC2sKaR6OTgnDzoPwpHdpmEMY2rM8CLT8DbStzvnwQPf51Z"
+}, {
+  firstName: 'Roger',
+  lastName: 'Rabbit',
+  email: 'roger-rabbit@email.com',
+  addressLine1: '123 Baker Street',
+  city: 'Paris',
+  state: 'ID',
+  zipCode: '88775',
+  timeZone: "Mountain",
+  avatarURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM1yFuZbX_ObFSnyaESvq_1a_CTqCLTMURXViN5RyrQvHUm814"
+}, {
+  firstName: 'Jack',
+  lastName: 'InTheBox',
+  email: 'jackinbox@email.com',
+  addressLine1: '123 Baker Street',
+  city: 'Paris',
+  state: 'ID',
+  zipCode: '88775',
+  timeZone: "Mountain",
+  avatarURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1vXjnr5RTywwY_6k8nOR7-Oz6B8ci4aNac-CGL9J3y3QRNLfo"
+}, {
+  firstName: 'John',
+  lastName: 'Jackson',
+  email: 'john-jackson@email.com',
+  addressLine1: "7643 Some Lane",
+  city: "Topeka",
+  state: "KS",
+  zipCode: "56788",
+  timeZone: "Central",
+  avatarURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6Mh-9HGP3BCegpacxN9MtJFDCtextZ6c9cVC4xdnsuA0WsJgp5A"
+}, {
+  firstName: 'Jennifer',
+  lastName: 'Ownerson',
+  email: 'jennifern@ownerson.com',
+  addressLine1: "7643 Some Lane",
+  city: "Kansas City",
+  state: "KS",
+  zipCode: "56788",
+  timeZone: "Central",
+  avatarURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7O1-5ygT98zoHdS26QzqvClNlzTGqeP9fRIj-KTJKwI15QgSF"
+}]);
 
 /***/ }),
 
