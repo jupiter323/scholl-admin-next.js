e2640123d74305b082b5a2bdc677fb851b39bea6
xinglong717 <xinglong717@gmail.com>
2019-12-13 23:55:02 +1000

    fixed edit modal for ClassRoom

--- components/Class/ListPage/components/ClassDetailModal/index.js
+++ components/Class/ListPage/components/ClassDetailModal/index.js
@@ -15,21 +15,141 @@ import { nestedEditFieldValidation } from '../../../../utils/fieldValidation';
 class ClassDetailModal extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      deleteLocationModalOpen: false,
+      newLocationModalOpen: false,
+      editLocationModalOpen: false,
+      pendingLocationDelete: {},
+      activeLocation: {},
+      dropdownIsOpen: false,
+      dropdownIndex: null,
+      originalClassRoom: {
+        accountInfo: {
+          firstName: '',
+          lastName: '',
+          email: '',
+          gender: '',
+        },
+        contactInfo: {
+          phone: '',
+          streetAddress: '',
+          city: '',
+          state: '',
+          zip: '',
+        },
+        locations: [],
+      },
+      updatedClassRoom: {
+        accountInfo: {
+          firstName: '',
+          lastName: '',
+          email: '',
+          gender: '',
+        },
+        contactInfo: {
+          phone: '',
+          streetAddress: '',
+          city: '',
+          state: '',
+          zip: '',
+        },
+        locations: [],
+      },
+      validation: {
+        accountInfo: {
+          firstName: true,
+          lastName: true,
+          email: true,
+          gender: true,
+        },
+      },
+    }
   }
+
+  componentDidMount() {
+    const { classroom: { id, basicInfo, accountInfo, contactInfo, locations } = {} } = this.props;
+    const updatedClassRoom = { id, basicInfo, accountInfo, contactInfo, locations };
+    const { originalClassRoom: originalClassRoomState } = this.state;
+    const originalClassRoom = update(originalClassRoomState, {
+      $merge: updatedClassRoom,
+    });
+    this.setState({ originalClassRoom, updatedClassRoom }); // eslint-disable-line
+  }
+
+  // This resets the component state to reflect the details of the next classroom the user clicks on
+  componentWillReceiveProps = (nextProps) => {
+    if ((!this.state.originalClassRoom || nextProps.classroom.id !== this.state.originalClassRoom.id)) {
+      const { classroom: { id, basicInfo, accountInfo, contactInfo, locations } = {} } = nextProps;
+      const updatedClassRoom = { id, basicInfo, accountInfo, contactInfo, locations };
+      const { originalClassRoom: originalClassRoomState } = this.state;
+      const originalClassRoom = update(originalClassRoomState, {
+        $merge: nextProps.classroom,
+      });
+      this.setState({ originalClassRoom, updatedClassRoom });
+    }
+  }
+
+  initialclassroomMount = () => this.state.originalClassRoom.id !== this.props.classroom.id;
+
+  renderModalHeader = () => {
+    const { classroom: { accountInfo: { lastName: initialLastName, firstName: initialFIrstName, email: initialEmail } } } = this.props;
+    const { updatedClassRoom: { accountInfo: { lastName: updatedLastName, firstName: updatedFirstName, email: updatedEmail } } } = this.state;
+    let lastName;
+    let firstName;
+    let email;
+    if (this.initialclassroomMount()) {
+      lastName = initialLastName;
+      firstName = initialFIrstName;
+      email = initialEmail;
+    } else {
+      lastName = updatedLastName;
+      firstName = updatedFirstName;
+      email = updatedEmail;
+    }
+    return (
+      <React.Fragment>
+        <h4 className="h3">{lastName}, {firstName}</h4>
+        <a href={`mailto:${email}`}>{email}</a>
+      </React.Fragment>
+    );
+  }
+
+  // If all the fields are valid, we construct a post body and call onSaveNewLocation passed down from the container level
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const { updatedClassRoom: { id, basicInfo, accountInfo, contactInfo, locations } } = this.state;
+    const { onSaveLocationError, onSaveInstructorChanges, onClose } = this.props;
+    // NOTE: Swap out what instance of valid is active if you want to test saving a new location without worrying about validation
+    // const valid = true;
+    const valid = await nestedEditFieldValidation(this.state, this.state.updatedClassRoom, this.onSetValidation, (validation) => console.warn('validation', validation));
+    if (!valid) {
+      // return onSaveLocationError();
+      console.warn('not valid');
+    }
+    const postBody = { id, basicInfo, accountInfo, contactInfo, locations };
+    onSaveInstructorChanges(postBody);
+    console.warn('stubbed out save function');
+    onClose();
+  }
+
+  onCancelChanges = () => {
+    const { onClose } = this.props;
+    const { updatedClassRoom, originalClassRoom } = this.state;
+    this.setState({ updatedClassRoom: originalClassRoom }, onClose());
+  }
+
   render() {
-    const { open } = this.props;
+    const { open,onOpenDeleteModal, deleteModalOpen } = this.props;
+    const { dropdownIsOpen, dropdownIndex, activeLocation, deleteLocationModalOpen, pendingLocationDelete, newLocationModalOpen, editLocationModalOpen,
+      updatedClassRoom: {accountInfo: updatedAccountInfo, contactInfo: updatedContactInfo, locations: updatedLocations,},
+    } = this.state;
     return (
       <Portal selector="#modal">
         {open && (
           <div className="overlay">
             <ClickOffComponentWrapper
               onOuterClick={this.onCancelChanges}
-              nestedModals={
-                deleteModalOpen ||
-                deleteLocationModalOpen ||
-                editLocationModalOpen ||
-                newLocationModalOpen
-              }
+              nestedModals={ deleteModalOpen || deleteLocationModalOpen || editLocationModalOpen || newLocationModalOpen }
             >
               <div
                 id="modal_user_edit"
@@ -70,16 +190,12 @@ class ClassDetailModal extends React.Component {
                       <div className="row mb-0">
                         <div className="col s12 l6">
                           <AccountInfo
-                            state={
-                              this.initialInstructorMount()
-                                ? accountInfo
-                                : updatedAccountInfo
-                            }
+                            state={ this.initialclassroomMount()? accountInfo: updatedAccountInfo }
                             handleDetailsChange={this.handleDetailsChange}
                           />
                           <ContactInfo
                             state={
-                              this.initialInstructorMount()
+                              this.initialclassroomMount()
                                 ? contactInfo
                                 : updatedContactInfo
                             }
@@ -88,11 +204,7 @@ class ClassDetailModal extends React.Component {
                         </div>
                         <div className="col s12 l6">
                           <Locations
-                            state={
-                              this.initialInstructorMount()
-                                ? locations
-                                : updatedLocations
-                            }
+                            state={ this.initialclassroomMount()? locations : updatedLocations }
                             handleDetailsChange={this.handleDetailsChange}
                             dropdownIsOpen={dropdownIsOpen}
                             dropdownIndex={dropdownIndex}
@@ -104,21 +216,11 @@ class ClassDetailModal extends React.Component {
                             onSetDropdown={this.onSetDropdown}
                             onCloseDropdown={this.onCloseDropdown}
                             onOpenNewLocationModal={this.onOpenNewLocationModal}
-                            onOpenEditLocationModal={
-                              this.onOpenEditLocationModal
-                            }
-                            onCloseNewLocationModal={
-                              this.onCloseNewLocationModal
-                            }
-                            onCloseEditLocationModal={
-                              this.onCloseEditLocationModal
-                            }
-                            onOpenDeleteLocationModal={
-                              this.onOpenDeleteLocationModal
-                            }
-                            onCloseDeleteLocationModal={
-                              this.onCloseDeleteLocationModal
-                            }
+                            onOpenEditLocationModal={ this.onOpenEditLocationModal }
+                            onCloseNewLocationModal={ this.onCloseNewLocationModal}
+                            onCloseEditLocationModal={ this.onCloseEditLocationModal}
+                            onOpenDeleteLocationModal={ this.onOpenDeleteLocationModal }
+                            onCloseDeleteLocationModal={ this.onCloseDeleteLocationModal}
                           />
                         </div>
                       </div>
@@ -148,6 +250,39 @@ class ClassDetailModal extends React.Component {
             </ClickOffComponentWrapper>
           </div>
         )}
+        <style jsx>
+          {`
+              .overlay {
+                position: fixed;
+                background-color: rgba(0, 0, 0, 0.7);
+                top: 0;
+                right: 0;
+                bottom: 0;
+                left: 0;
+                z-index: 1999;
+              }
+              .card-modal {
+                margin: 0;
+                border-radius: 6px;
+              }
+              .modal {
+                display: block;
+                background-color: white;
+                position: absolute;
+                top: 10%;
+                right: 10%;
+                left: 10%;
+                box-shadow: 0 24px 38px 3px rgba(0, 0, 0, 0.14), 0 9px 46px 8px rgba(0, 0, 0, 0.12), 0 11px 15px -7px rgba(0, 0, 0, 0.2);
+              }
+              .modal-custom {
+                opacity: 1;
+                visibility: visible;
+              }
+              .modal-footer {
+                background-color: white;
+              }
+            `}
+        </style>
       </Portal>
     );
   }
--- components/Class/utils/sampleClass.js
+++ components/Class/utils/sampleClass.js
@@ -102,6 +102,28 @@ const sampleClass = [
         correctAnswers: "37",
         totalAnswers: "52"
       }
+    },
+    accountInfo: {
+      lastName: 'Admin',
+      firstName: 'Company',
+      email: 'test2@example.com',
+      gender: 'M'
+    },
+    contactInfo: {
+      phone: '1234567890',
+      streetAddress: '1234 Test Road',
+      city: 'Austin',
+      state: 'TX',
+      zip: '78751'
+    },
+    basicInfo: {
+      activeStudents: 15,
+      pastStudents: 24,
+      unactivatedStudents: 29,
+      averageImprovement: 185,
+      averageInitialScore: 1037,
+      averageFinalScore: 1218,
+      studentsAchievingTargetScore: 12
     }
   },
   {
@@ -207,6 +229,28 @@ const sampleClass = [
         correctAnswers: "37",
         totalAnswers: "52"
       }
+    },
+    accountInfo: {
+      lastName: 'Admin',
+      firstName: 'Company',
+      email: 'test2@example.com',
+      gender: 'M'
+    },
+    contactInfo: {
+      phone: '1234567890',
+      streetAddress: '1234 Test Road',
+      city: 'Austin',
+      state: 'TX',
+      zip: '78751'
+    },
+    basicInfo: {
+      activeStudents: 15,
+      pastStudents: 24,
+      unactivatedStudents: 29,
+      averageImprovement: 185,
+      averageInitialScore: 1037,
+      averageFinalScore: 1218,
+      studentsAchievingTargetScore: 12
     }
   }
 ];
