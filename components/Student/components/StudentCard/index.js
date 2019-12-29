bd10070e1f59eb77138097e948a913f517252d86
Christian Chavarro <chavarro.christian@gmail.com>
2019-12-29 15:44:44 -0600

    Configured api and student card to handle student stats

--- components/Student/components/StudentCard/index.js
+++ components/Student/components/StudentCard/index.js
@@ -2,29 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Doughnut } from 'react-chartjs-2';
 import EditModal from './components/EditModal';
-import ClickOffComponentWrapper from '../../../ClickOffComponentWrapper';
+
 const data = (current, target, initial) => ({
   datasets: [{
-    data: [current-initial, (target-initial) - (current-initial) ],
+    data: [current - initial, (target - initial) - (current - initial)],
     backgroundColor: [
       '#0085ce',
       '#eaeaea',
     ],
   }],
-})
+});
 
 class StudentCard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            editModalOpen: false,
-            cloneModalOpen: false,
-            showOwnerModalOpen: false,
-            deleteModalOpen: false,
-            activateAccountModalOpen: false,
-            assignInstructorModalOpen: false,
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      editModalOpen: false,
+      cloneModalOpen: false,
+      showOwnerModalOpen: false,
+      deleteModalOpen: false,
+      activateAccountModalOpen: false,
+      assignInstructorModalOpen: false,
+    };
+  }
+
+    onOpenEditModal = () => {
+      this.props.onCloseDropdown();
+      this.setState({ editModalOpen: true });
     }
+    onCloseEditModal = () => this.setState({ editModalOpen: false })
 
     handleDropdownClick = (event) => {
       const { onSetDropdown, onCloseDropdown, dropdownIsOpen, index } = this.props;
@@ -35,149 +41,146 @@ class StudentCard extends React.Component {
       return onSetDropdown(index);
     }
 
-    onOpenEditModal = () => {
-      this.props.onCloseDropdown();
-      this.setState({editModalOpen: true});
-    }
-    onCloseEditModal = () => this.setState({editModalOpen: false})
-
     render() {
-    const { onHandleStudentCard,onSaveStudentChanges, onDeleteStudent, onCloneStudent, index, dropdownIndex,student, student: {id, active, tutor, testScores: { initialScore, currentScore }, courseContext: {targetScore}, studentInformation: { firstName, lastName },
-        emailAddress: { email }},dropdownIsOpen }= this.props;
-    const { editModalOpen } = this.state;
-    return (
-      <React.Fragment>
-      <EditModal open={editModalOpen} onSaveStudentChanges = {onSaveStudentChanges} onCloseEditModal={this.onCloseEditModal} student={student} handleDetailsChange={this.handleDetailsChange}/>
+      const { onHandleStudentCard, onSaveStudentChanges, onDeleteStudent, onCloneStudent, index, dropdownIndex, student, student: { id, active, tutor, stats, testScores: { initialScore, currentScore }, courseContext: { targetScore }, studentInformation: { firstName, lastName },
+        emailAddress: { email } }, dropdownIsOpen } = this.props;
+      const { editModalOpen } = this.state;
+      return (
+        <React.Fragment>
+          <EditModal open={editModalOpen} onSaveStudentChanges={onSaveStudentChanges} onCloseEditModal={this.onCloseEditModal} student={student} handleDetailsChange={this.handleDetailsChange} />
 
-      <div className="card-main-col col s12 m8 l7 xl5" id={id}>
-          <div className={ active? "card-main card-location card card-large" : "card-main card-location card-disabled card-large card"}>
+          <div className="card-main-col col s12 m8 l7 xl5" id={id}>
+            <div className={active ? "card-main card-location card card-large" : "card-main card-location card-disabled card-large card"}>
 
-        <div className="owner-box card-panel card-panel-panel card-panel-large" style={{ backgroundColor: active ? '#0085ce' : '#9b9b9b', color: '#fff' }}>
-          <div className="card-panel-row row">
-          <div className="col s9">
-            <div className="user-block">
-              <div className="user-circle" style={{ backgroundColor: active ? '#14b14b' : '#808080', color: '#fff', height: '45px' }}>
-                <img src="#" alt="" />
-              </div>
-              <div className="user-text" style={{ color: '#fff' }}>
-                <h4 className="h3"><a href="#" value={student} onClick={() => onHandleStudentCard({index})}>{lastName}, {firstName}</a></h4>
-                <a href={`mailto:${email}`}>{email}</a>
-              </div>
-            </div>
-          </div>
-          <div className="col s2 right-align">
-            <span className="block-icon">
-              <i className="icon-user"></i>
-              <span className="text-icon">Student</span>
-            </span>
-          </div>
-          <div className="col s1 right-align">
-            <div className="row icons-row">
-              <div className="dropdown-block col">
-                <a
-                href='#'
-                className='dropdown-trigger btn'
-                data-target='dropdown01'
-                onClick={this.handleDropdownClick}>
-                <i className="material-icons dots-icon">more_vert</i>
-                </a>
-                <If condition={dropdownIsOpen && dropdownIndex === index}>
-                <ul id='dropdown01' className='dropdown-content dropdown-wide' style={{display: "block", opacity: '1', transform: 'scaleX(1) scaleY(1)'}}>
-                  <li>
-                    <a href="#" className="modal-trigger link-block" onClick={this.onOpenEditModal}>Edit</a>
-                  </li>
-                  <li><a href="#!" onClick={() => onCloneStudent(index)}>Clone</a></li>
-                  <li><a href="#!">Show Owner</a></li>
-                  <li><a href="#!" onClick={() => onDeleteStudent(index)}>Delete</a></li>
-                </ul>
-                </If>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* card content */}
-      <div className="card-content" ref={this.studentCard}>
-          <div className="row d-flex align-items-center mb-0">
-            <div className="col s12 m6">
-              <div className="chart-container chart-container-large">
-                <div className="chart-holder">
-                  <If condition={active}>
-                    <Doughnut
-                      data={() => data(currentScore, targetScore, initialScore)}
-                      height={210}
-                      options={{
-                        circumference: 1.45 * Math.PI,
-                        rotation: -3.85,
-                        cutoutPercentage: 60,
-                        tooltips: false,
-                      }}
-                    />
-                  </If>
-                  <span className="chart-value" style={{backgroundColor: "#0085ce", bottom: "7px"}}><span data-count-up data-start-val={initialScore} data-end-val={currentScore} data-duration="1">{currentScore}</span></span>
-                </div>
-                <div className="chart-row">
-                  <div className="chart-col chart-start">
-                    <span className="amount" style={{color: '#0085ce'}}>{initialScore}</span>
-                    <span className="amount-text">&nbsp; initial</span>
+              <div className="owner-box card-panel card-panel-panel card-panel-large" style={{ backgroundColor: active ? '#0085ce' : '#9b9b9b', color: '#fff' }}>
+                <div className="card-panel-row row">
+                  <div className="col s9">
+                    <div className="user-block">
+                      <div className="user-circle" style={{ backgroundColor: active ? '#14b14b' : '#808080', color: '#fff', height: '45px' }}>
+                        <img src="#" alt="" />
+                      </div>
+                      <div className="user-text" style={{ color: '#fff' }}>
+                        <h4 className="h3"><a href="#" value={student} onClick={() => onHandleStudentCard({ index })}>{lastName}, {firstName}</a></h4>
+                        <a href={`mailto:${email}`}>{email}</a>
+                      </div>
+                    </div>
                   </div>
-                  <div className="chart-col chart-end">
-                    <span className="amount">{targetScore}</span>
-                    <span className="amount-text">&nbsp; target</span>
+                  <div className="col s2 right-align">
+                    <span className="block-icon">
+                      <i className="icon-user"></i>
+                      <span className="text-icon">Student</span>
+                    </span>
+                  </div>
+                  <div className="col s1 right-align">
+                    <div className="row icons-row">
+                      <div className="dropdown-block col">
+                        <a
+                          href="#"
+                          className="dropdown-trigger btn"
+                          data-target="dropdown01"
+                          onClick={this.handleDropdownClick}
+                        >
+                          <i className="material-icons dots-icon">more_vert</i>
+                        </a>
+                        <If condition={dropdownIsOpen && dropdownIndex === index}>
+                          <ul id="dropdown01" className="dropdown-content dropdown-wide" style={{ display: "block", opacity: '1', transform: 'scaleX(1) scaleY(1)' }}>
+                            <li>
+                              <a href="#" className="modal-trigger link-block" onClick={this.onOpenEditModal}>Edit</a>
+                            </li>
+                            <li><a href="#!" onClick={() => onCloneStudent(index)}>Clone</a></li>
+                            <li><a href="#!">Show Owner</a></li>
+                            <li><a href="#!" onClick={() => onDeleteStudent(index)}>Delete</a></li>
+                          </ul>
+                        </If>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="col s12 m6">
-              <ul className="points-list-bordered">
-                <li className="style-green">
-                  <span className="badge-circle">61</span>
-                  <span className="point-text">complete</span>
-                </li>
-                <li className="style-red">
-                  <span className="badge-circle">7</span>
-                  <span className="point-text">overdue</span>
-                </li>
-                <li className="style-orange">
-                  <span className="badge-circle">3</span>
-                  <span className="point-text">practice tests</span>
-                </li>
-                <li className="style-blue-lighter">
-                  <span className="badge-circle"><span>17</span> <span className="of">of 23</span></span>
-                  <span className="point-text">sessions</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="card-inner-row">
-            <div className="row-holder">
-              <ul className="category-list">
-                <li className="category-box">
-                  <span className="category-badge badge-purple"></span>
-                  <span className="category-text">{tutor}</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      </div>
-     {/* </div> */}
-    </React.Fragment>
-)}};
 
-  StudentCard.propTypes = {
-    student: PropTypes.object.isRequired,
-    onHandleStudentCard: PropTypes.func.isRequired,
-    dropdownIsOpen: PropTypes.bool.isRequired,
-    index: PropTypes.number.isRequired,
-    onDeleteStudent: PropTypes.func.isRequired,
-    onCloneStudent: PropTypes.func.isRequired,
-    onSetDropdown: PropTypes.func.isRequired,
-    onCloseDropdown: PropTypes.func.isRequired,
-    dropdownIndex: PropTypes.number,
-    onSaveStudentChanges:PropTypes.func.isRequired,
-  };
+              {/* card content */}
+              <div className="card-content" ref={this.studentCard}>
+                <div className="row d-flex align-items-center mb-0">
+                  <div className="col s12 m6">
+                    <div className="chart-container chart-container-large">
+                      <div className="chart-holder">
+                        <If condition={active}>
+                          <Doughnut
+                            data={() => data(currentScore, targetScore, initialScore)}
+                            height={210}
+                            options={{
+                              circumference: 1.45 * Math.PI,
+                              rotation: -3.85,
+                              cutoutPercentage: 60,
+                              tooltips: false,
+                            }}
+                          />
+                        </If>
+                        <span className="chart-value" style={{ backgroundColor: "#0085ce", bottom: "7px" }}><span data-count-up data-start-val={initialScore} data-end-val={currentScore} data-duration="1">{currentScore}</span></span>
+                      </div>
+                      <div className="chart-row">
+                        <div className="chart-col chart-start">
+                          <span className="amount" style={{ color: '#0085ce' }}>{initialScore}</span>
+                          <span className="amount-text">&nbsp; initial</span>
+                        </div>
+                        <div className="chart-col chart-end">
+                          <span className="amount">{targetScore}</span>
+                          <span className="amount-text">&nbsp; target</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col s12 m6">
+                    <ul className="points-list-bordered">
+                      <li className="style-green">
+                        <span className="badge-circle">{stats.complete}</span>
+                        <span className="point-text">complete</span>
+                      </li>
+                      <li className="style-red">
+                        <span className="badge-circle">{stats.overdue}</span>
+                        <span className="point-text">overdue</span>
+                      </li>
+                      <li className="style-orange">
+                        <span className="badge-circle">{stats.practiceTests}</span>
+                        <span className="point-text">practice tests</span>
+                      </li>
+                      <li className="style-blue-lighter">
+                        <span className="badge-circle"><span>{stats.sessionsComplete}</span> <span className="of">of {stats.totalSessions}</span></span>
+                        <span className="point-text">sessions</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card-inner-row">
+                  <div className="row-holder">
+                    <ul className="category-list">
+                      <li className="category-box">
+                        <span className="category-badge badge-purple"></span>
+                        <span className="category-text">{tutor}</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* </div> */}
+        </React.Fragment>
+      );
+    }
+}
+
+StudentCard.propTypes = {
+  student: PropTypes.object.isRequired,
+  onHandleStudentCard: PropTypes.func.isRequired,
+  dropdownIsOpen: PropTypes.bool.isRequired,
+  index: PropTypes.number.isRequired,
+  onDeleteStudent: PropTypes.func.isRequired,
+  onCloneStudent: PropTypes.func.isRequired,
+  onSetDropdown: PropTypes.func.isRequired,
+  onCloseDropdown: PropTypes.func.isRequired,
+  dropdownIndex: PropTypes.number,
+  onSaveStudentChanges: PropTypes.func.isRequired,
+};
 
 export default StudentCard;
--- components/Student/index/api.js
+++ components/Student/index/api.js
@@ -1,3 +1,5 @@
+/* eslint-disable camelcase */
+/* eslint-disable jsx-control-statements/jsx-jcs-no-undef */
 const API_URL = process.env.API_URL;
 
 export const fetchStudentsApi = () =>
@@ -9,42 +11,36 @@ export const fetchStudentsApi = () =>
   })
     .then(res => res.json())
     .then(({ data }) => {
-      const formattedStudents = data.students.map(student => ({
-          id: student.id,
-          active: false,
-          studentInformation: {
-            firstName: student.first_name,
-            lastName: student.last_name,
-          },
-          contactInformation: {
-            phone: '7865645859',
-            addressLine1: '1234 Test Lane',
-            addressLine2: '',
-            city: 'Austin',
-            state: 'TX',
-            zipCode: '78757',
-          },
-          emailAddress: {
-            email: student.email,
-          },
-          location: {
-            locations: [
-              {
-                locationNickname: 'Disneyworld  Tutoring',
-                locationName: 'House Of Mouse',
-              },
-              {
-                locationNickname: 'TutorZone Austin',
-                locationName: 'We Teach Real Good',
-              },
-              {
-                locationNickname: 'TutorZone Miami',
-                locationName: 'We Teach Even Better',
-              },
-            ],
-          },
-        }));
-      return formattedStudents;
+      const students = data.students.map(student => ({
+        id: student.user_address.user_id,
+        active: student.active === 1,
+        studentInformation: {
+          firstName: student.first_name,
+          lastName: student.last_name,
+        },
+        contactInformation: {
+          phone: student.user_address.phone,
+          addressLine1: student.user_address.address,
+          addressLine2: '',
+          city: student.user_address.city,
+          state: student.user_address.state,
+          zipCode: student.user_address.zip,
+        },
+        emailAddress: {
+          email: student.email,
+        },
+        location: {
+          locations: student.user_locations,
+        },
+        stats: {
+          complete: student.stats.complete,
+          overdue: student.stats.overdue,
+          pracitceTests: student.stats.practice_tests,
+          sessionsComplete: student.stats.sessions_complete,
+          totalSessions: student.stats.total_sessions,
+        },
+      }));
+      return students;
     });
 
 export const addNewStudentApi = student => {
@@ -80,127 +76,127 @@ export const updateStudentActivationApi = (body) =>
     .then(res => res.json())
     .catch(err => err);
 
-  export const updateStudentAddressApi = (body) =>
-    fetch(`${API_URL}/api/commands/update-student-address`,{
-      method:'PATCH',
-      headers:{
-        Accept:"application/json",
-        "Content-Type":"application/json",
-      },
-      body:JSON.stringify(body)
-    })
+export const updateStudentAddressApi = (body) =>
+  fetch(`${API_URL}/api/commands/update-student-address`, {
+    method: 'PATCH',
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
     .then(res => res.json())
     .catch(err => err);
 
-  export const updateStudentCityApi = (body) =>
-    fetch(`${API_URL}/api/commands/update-student-city`,{
-      method:'PATCH',
-      headers:{
-        Accept:"application/json",
-        "Content-Type":"application/json",
-      },
-      body:JSON.stringify(body)
-    })
-    .then ( res => res.json())
-    .catch( err => err);
+export const updateStudentCityApi = (body) =>
+  fetch(`${API_URL}/api/commands/update-student-city`, {
+    method: 'PATCH',
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then(res => res.json())
+    .catch(err => err);
 
-  export const updateStudentEmailApi = (body) =>
-    fetch(`${API_URL}/api/commands/update-student-email`,{
-      method:'PATCH',
-      headers:{
-        Accept:"application/json",
-        "Content-Type":"application/json",
-      },
-      body:JSON.stringify(body)
-    })
-    .then ( res => res.json())
-    .catch( err => err);
+export const updateStudentEmailApi = (body) =>
+  fetch(`${API_URL}/api/commands/update-student-email`, {
+    method: 'PATCH',
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then(res => res.json())
+    .catch(err => err);
 
-  export const updateStudentFirstNameApi = (body) =>
-    fetch(`${API_URL}/api/commands/update-student-first-name`,{
-      method:'PATCH',
-      headers:{
-        Accept:"application/json",
-        "Content-Type":"application/json",
-      },
-      body:JSON.stringify(body)
-    })
-    .then ( res => res.json())
-    .catch( err => err);
+export const updateStudentFirstNameApi = (body) =>
+  fetch(`${API_URL}/api/commands/update-student-first-name`, {
+    method: 'PATCH',
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then(res => res.json())
+    .catch(err => err);
 
-  export const updateStudentLastNameApi = (body) =>
-    fetch(`${API_URL}/api/commands/update-student-last-name`,{
-      method:'PATCH',
-      headers:{
-        Accept:"application/json",
-        "Content-Type":"application/json",
-      },
-      body:JSON.stringify(body)
-    })
-    .then ( res => res.json())
-    .catch( err => err);
+export const updateStudentLastNameApi = (body) =>
+  fetch(`${API_URL}/api/commands/update-student-last-name`, {
+    method: 'PATCH',
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then(res => res.json())
+    .catch(err => err);
 
-  export const updateStudentPhoneApi = (body) =>
-    fetch(`${API_URL}/api/commands/update-student-phone`,{
-      method:'PATCH',
-      headers:{
-        Accept:"application/json",
-        "Content-Type":"application/json",
-      },
-      body:JSON.stringify(body)
-    })
-    .then ( res => res.json())
-    .catch( err => err);
+export const updateStudentPhoneApi = (body) =>
+  fetch(`${API_URL}/api/commands/update-student-phone`, {
+    method: 'PATCH',
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then(res => res.json())
+    .catch(err => err);
 
-  export const updateStudentStateApi = (body) =>
-    fetch(`${API_URL}/api/commands/update-student-state`,{
-      method:'PATCH',
-      headers:{
-        Accept:"application/json",
-        "Content-Type":"application/json",
-      },
-      body:JSON.stringify(body)
-    })
-    .then ( res => res.json())
-    .catch( err => err);
+export const updateStudentStateApi = (body) =>
+  fetch(`${API_URL}/api/commands/update-student-state`, {
+    method: 'PATCH',
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then(res => res.json())
+    .catch(err => err);
 
-  export const updateStudentZipApi = (body) =>
-    fetch(`${API_URL}/api/commands/update-student-zip`,{
-      method:'PATCH',
-      headers:{
-        Accept:"application/json",
-        "Content-Type":"application/json",
-      },
-      body:JSON.stringify(body)
-    })
-    .then ( res => res.json())
-    .catch( err => err);
+export const updateStudentZipApi = (body) =>
+  fetch(`${API_URL}/api/commands/update-student-zip`, {
+    method: 'PATCH',
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then(res => res.json())
+    .catch(err => err);
 
-  export const deleteStudentApi = id =>
-    fetch(`${API_URL}/api/commands/delete-student`, {
-        method: "DELETE",
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ student_id: id }),
-    })
-        .then(res => res.json())
-        .catch(err => err);
+export const deleteStudentApi = id =>
+  fetch(`${API_URL}/api/commands/delete-student`, {
+    method: "DELETE",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ student_id: id }),
+  })
+    .then(res => res.json())
+    .catch(err => err);
 
 
 export default [
-      fetchStudentsApi,
-      addNewStudentApi,
-      deleteStudentApi,
-      updateStudentActivationApi,
-      updateStudentAddressApi,
-      updateStudentCityApi,
-      updateStudentEmailApi,
-      updateStudentFirstNameApi,
-      updateStudentLastNameApi,
-      updateStudentPhoneApi,
-      updateStudentStateApi,
-      updateStudentZipApi,
-    ];
+  fetchStudentsApi,
+  addNewStudentApi,
+  deleteStudentApi,
+  updateStudentActivationApi,
+  updateStudentAddressApi,
+  updateStudentCityApi,
+  updateStudentEmailApi,
+  updateStudentFirstNameApi,
+  updateStudentLastNameApi,
+  updateStudentPhoneApi,
+  updateStudentStateApi,
+  updateStudentZipApi,
+];
 
