cb30de80b4f301f8f0c872c042f035778a28d41f
xinglong717 <xinglong717@gmail.com>
2020-01-15 02:24:10 +1000

    fixed NewTestModal Style

--- components/Student/DetailTestList/components/TestModal/index.js
+++ components/Student/DetailTestList/components/TestModal/index.js
@@ -1,15 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Portal from "../../../../Portal";
-
+import getValueFromState from "../../../../utils/getValueFromState";
+import Dropdown from "../../../../FormComponents/Dropdown";
 
 class NewTestModal extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      version: "SAT Pratice Test #1",
+      timeAssignedFor: '',
+      versionOptions: [
+        {
+          label: "SAT Pratice Test #1",
+          value: "SAT Pratice Test #1"
+        },
+        {
+          label: "SAT Pratice Test #2",
+          value: "SAT Pratice Test #2"
+        },
+        {
+          label: "SAT Pratice Test #3",
+          value: "SAT Pratice Test #3"
+        }
+      ]
+    };
   }
 
+  handleDetailsChange = (event, name) => {};
+
   render() {
-      const { open,onClose } = this.props;
+    const { open, onClose } = this.props;
+    const { versionOptions, version,timeAssignedFor } = this.state;
     return (
       <Portal selector="#modal">
         {open && (
@@ -24,16 +46,14 @@ class NewTestModal extends React.Component {
                     <div className="card-modal card-main card">
                       <div
                         className="card-panel card-panel-title"
-                        style={{ backgroundColor: "#408e49", color: "#fff" }}
+                        style={{ backgroundColor: "#E74021", color: "#fff" }}
                       >
                         <div className="card-panel-row row">
                           <div className="col">
                             <h2>
                               <span className="heading-holder">
-                                <i className="icon-members" />
-                                <span className="heading-block">
-                                  Test
-                                </span>
+                                <i className="icon-courses" />
+                                <span className="heading-block">New Test</span>
                               </span>
                             </h2>
                           </div>
@@ -50,6 +70,54 @@ class NewTestModal extends React.Component {
                       </div>
                       <div className="card-content">
                         <div className="card-body">
+                          <div className="row mb-0">
+                            <div className="col s12">
+                              <div className="input-field">
+                                <Dropdown
+                                  value={getValueFromState(
+                                    version,
+                                    versionOptions
+                                  )}
+                                  onChange={event =>
+                                    this.handleDetailsChange(event, "version")
+                                  }
+                                  options={versionOptions}
+                                  label="Version"
+                                  stateKey="version"
+                                  dropdownKey="version"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                          <div className="row mb-0">
+                            <div className="col s12">
+                              <div className="datepicker-field input-field">
+                                <i className="icon-clock2"></i>
+                                <input
+                                  type="text"
+                                  className="timepicker"
+                                  id="assignFor"
+                                  value={timeAssignedFor}
+                                  onChange={event =>
+                                    this.handleDetailsChange(
+                                      event,
+                                      "timeAssignedFor"
+                                    )
+                                  }
+                                />
+                                <label
+                                  className={
+                                    timeAssignedFor.length
+                                      ? "label active"
+                                      : "label"
+                                  }
+                                  htmlFor="detail_estimated_time"
+                                >
+                                  AssignFor
+                                </label>
+                              </div>
+                            </div>
+                          </div>
                         </div>
                         <div className="modal-footer">
                           <a
@@ -116,7 +184,7 @@ class NewTestModal extends React.Component {
 
 NewTestModal.propTypes = {
   open: PropTypes.bool.isRequired,
-  onClose:PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired
 };
 
 export default NewTestModal;
