7a01285eb7b8e28ff63f5b38e1c66e1b2445628a
yuraivanov <yuraivanov.dev@gmail.com>
2020-03-21 05:29:04 -0700

    fixed progressbar style

--- components/Student/DetailTestScorePage/components/SectionScoreCard/index.js
+++ components/Student/DetailTestScorePage/components/SectionScoreCard/index.js
@@ -2,82 +2,85 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Doughnut } from "react-chartjs-2";
 
-const readingAndWritingData = (totalScore, totalPossible) => ({
-  datasets: [
-    {
-      data: [totalPossible, totalPossible - totalScore],
-      backgroundColor: ["#51b349", "rgb(234, 234, 234)"]
-    }
-  ]
-});
-const mathData = (totalScore, totalPossible) => ({
-  datasets: [
-    {
-      data: [totalPossible, totalPossible - totalScore],
-      backgroundColor: ["#567ebf", "rgb(234, 234, 234)"]
-    }
-  ]
-});
+class SectionScoreCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bgColorList: [
+        {
+          id: "Writing and Language",
+          colorString: ["#51b349", "rgb(234, 234, 234)"]
+        },
+        {
+          id: "Reading",
+          colorString: ["#35a6af", "rgb(234, 234, 234)"]
+        },
+        {
+          id: "Math",
+          colorString: ["#567ebf", "rgb(234, 234, 234)"]
+        },
+      ]
+    };
+  }
+  getCircleBarData = (currentScore, totalPossible, name) => ({
+    datasets: [
+      {
+        data: [totalPossible,totalPossible - currentScore],
+        backgroundColor: this.getBgColorByName(name)
+      }
+    ]
+  });
 
-const SectionScoreCard = ({
-  sectionScores: { readingAndWriting, math, totalPossible }
-}) => (
-  <div className="col s12 l6 card-width-546">
-    <div className="card-block">
-      <h2>
-        Section Scores <span className="separator">|</span>
-        <span className="quantity">200 to 800</span>
-      </h2>
-      <div className="card-main-full card">
-        <div className="card-content">
-          <div className="several-charts-row row mb-0">
-            <div className="col s6 center-align">
-              <div className="chart-block chart-block-218">
-                <Doughnut
-                  data={() =>
-                    readingAndWritingData(readingAndWriting, totalPossible)
-                  }
-                  width={215}
-                  height={215}
-                  options={{
-                    cutoutPercentage: 80
-                  }}
-                />
-                <div className="chart-text">
-                  <span className="title">Reading &amp; Writing</span>
-                  <span className="value">{readingAndWriting}</span>
-                  <span className="description">(+80)</span>
-                </div>
-              </div>
-            </div>
-            <div className="col s6 center-align">
-              <div className="chart-block chart-block-218">
-                <Doughnut
-                  data={() => mathData(math, totalPossible)}
-                  width={215}
-                  height={215}
-                  options={{
-                    cutoutPercentage: 80
-                  }}
-                />
-                <div className="chart-text">
-                  <span className="title">Math</span>
-                  <span className="value">{math}</span>
-                  <span className="description">(+80)</span>
-                </div>
+  getBgColorByName = (name) => {
+    const { bgColorList } = this.state;
+    const ids = bgColorList.map(element => element.id);
+    const targetIndex = ids.findIndex(ids => ids === name);
+    return bgColorList[targetIndex].colorString;
+  }
+
+  mapSectionScores = () => {
+    return Object.values(this.props.sectionScores).map(score => (
+      <div className="col s6 center-align">
+        <div className="chart-block chart-block-218">
+          <Doughnut
+            data={() =>
+              this.getCircleBarData(score.current_score, 800, score.name)
+            }
+            width={215}
+            height={215}
+            options={{
+              cutoutPercentage: 80
+            }}
+          />
+          <div className="chart-text">
+            <span className="title">{score.name === 'Writing and Language' ? 'Writing & Language' : score.name}</span>
+            <span className="value">{score.current_score}</span>
+            <span className="description">(+{score.delta ? score.delta : 0})</span>
+          </div>
+        </div>
+      </div>
+    ));
+  }
+  render() {
+    return (
+      <div className="col s12 l6 card-width-546">
+        <div className="card-block">
+          <h2>
+            Section Scores <span className="separator">|</span>
+            <span className="quantity">200 to 800</span>
+          </h2>
+          <div className="card-main-full card">
+            <div className="card-content">
+              <div className="several-charts-row row mb-0">
+                {this.mapSectionScores()}
               </div>
             </div>
           </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-            nonummy nibh euismod tincidunt ut laoreet dolore.
-          </p>
         </div>
       </div>
-    </div>
-  </div>
-);
-
+    );
+  }
+}
 SectionScoreCard.propTypes = {
   sectionScores: PropTypes.object.isRequired
 };
