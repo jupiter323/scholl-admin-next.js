a624dc420be99450166157a7b5f11c32f0c6128e
yuraivanov <yuraivanov.dev@gmail.com>
2020-03-24 11:20:55 -0700

    fixed Lesson Card

--- components/Student/DetailLessonList/components/FullView/components/LessonCard/index.js
+++ components/Student/DetailLessonList/components/FullView/components/LessonCard/index.js
@@ -374,7 +374,6 @@ LessonCard.propTypes = {
   lesson: PropTypes.object.isRequired,
   user: PropTypes.object.isRequired,
   index: PropTypes.number.isRequired,
-  onChecked: PropTypes.func.isRequired
 };
 
 export default LessonCard;
