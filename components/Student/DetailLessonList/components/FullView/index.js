a567e393f5be7da6ff4d316aa72c5a3b7623f6b4
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-03-26 15:58:14 -0500

    centered cards in FullView

--- components/Student/DetailLessonList/components/FullView/index.js
+++ components/Student/DetailLessonList/components/FullView/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import LessonCard from './components/LessonCard';
-import Checkbox from './components/LessonCard/components/Checkbox';
+import React from "react";
+import PropTypes from "prop-types";
+import LessonCard from "../FullView/components/LessonCard";
+// eslint-disable-next-line
+import ClickOffComponentWrapper from "../../../../ClickOffComponentWrapper";
+import Checkbox from "./components/LessonCard/components/Checkbox";
 
 class FullView extends React.Component {
   mapLessons = () => {
     const { lessons, onCloneLesson, onDeleteLesson, user, onCheckLesson } = this.props;
-
     return lessons.map((lesson, index) => (
       <LessonCard
         // eslint-disable-next-line react/no-array-index-key
@@ -25,7 +26,6 @@ class FullView extends React.Component {
     const { lessons } = this.props;
 
 
-
     return (
       <div className="content-section">
         <div className="d-flex justify-content-between">
@@ -34,13 +34,11 @@ class FullView extends React.Component {
             <span>Check all</span>
           </label> */}
           <div>
-
             <Checkbox label="Check all" checkBoxId="checkall" onChecked={this.props.onCheckAll} type="pageCheckBox" />
           </div>
           <div>
-            <b> - {this.props.lessons.length} results -</b>
+            <b> - {lessons.length} results -</b>
           </div>
-
 
           <div className="dropdown-block col">
             <a
@@ -54,7 +52,7 @@ class FullView extends React.Component {
             </a>
           </div>
         </div>
-        <div className="row d-flex-content card-width-272">
+        <div className="row d-flex-content  justify-content-center card-width-auto">
           {this.mapLessons()}
         </div>
       </div>
