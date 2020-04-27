7f7439ac037a4bbbbe58dee80ae182b2363115ba
yuraivanov <yuraivanov.dev@gmail.com>
2020-04-27 13:03:15 -0700

    fixed UnitName

--- components/Student/ReadWorkBook/index.js
+++ components/Student/ReadWorkBook/index.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import PropTypes from "prop-types";
 import { setOpenActivePage, setIsVisibleTopBar } from "../index/actions";
-import { makeSelectActiveLesson } from "../index/selectors";
+import { makeSelectActiveLesson, makeSelectUnitFilterOptions } from "../index/selectors";
 import moment from "moment";
 
 
@@ -22,6 +22,21 @@ class ReadWorkBook extends React.Component {
         onSetIsVisibleTopBar(true);
         onSetOpenActivePage("");
     }
+
+    getUnitNameById = () => {
+        const { units } = this.props;
+        if (units && units.length !== 0) {
+            const unitIds = units.map(unit => unit.value);
+            const currentIndex = unitIds.findIndex(this.getUnitIndexMatchedUnitId);
+            const currentUnit = units[currentIndex];
+            return currentUnit.label;
+        } else {
+            return "Undefind UnitName"
+        }
+    }
+
+    getUnitIndexMatchedUnitId = unitId => unitId === this.props.lesson.unit_id;
+
     render() {
         const { user,
             lesson: { name, starting_page, ending_page, completed_at, assignTime, assignment_date, due_date, dueTime, description } } = this.props;
@@ -44,42 +59,41 @@ class ReadWorkBook extends React.Component {
                             <div className="icon-col col s1">
                                 <i className="icon-books"></i>
                             </div>
-                            <div className='col s9'>
-                                <p className="text-small" style={{ marginBottom: 0, fontSize: 18 }}>Unit2</p>
+                            <div className='col s7'>
+                                <p className="text-small" style={{ marginBottom: 0, fontSize: 18 }}>{this.getUnitNameById()}</p>
                                 <p className="text-large" style={{ marginBottom: 0, fontSize: 24 }}>{name}</p>
                                 <p style={{ fontSize: '16px' }}>p.{starting_page}{ending_page > starting_page ? `-${ending_page}` : ""}(Reading)</p>
                             </div>
-                            <div className="col s2">
-                                <div className="card-panel-text center-align">
-                                    <div><span className="text-large" style={{ marginBottom: 0, fontSize: 24 }}>{firstName} {lastName}</span>  <div className="position-top right-align" style={{ float: 'right' }}>
-                                        <div className="icons-row">
-                                            <div className="dropdown-block col">
+                            <div className="col s4">
+                                <div className="position-top right-align" style={{ float: 'right' }}>
+                                    <div className="icons-row">
+                                        <div className="dropdown-block col">
+                                            <p className="text-large" style={{ fontSize: 24 }}>{firstName} {lastName}
                                                 <i className="material-icons dots-icon">more_vert</i>
-                                            </div>
-                                            <a
-                                                href="#"
-                                                className="icon-close"
-                                                onClick={this.onSetActivePage}
-                                                style={{ color: 'white' }}
-                                            ></a>
+                                                <a
+                                                    href="#"
+                                                    className="icon-close"
+                                                    onClick={this.onSetActivePage}
+                                                    style={{ color: 'white', fontSize: 12 }}
+                                                ></a></p>
                                         </div>
-                                    </div></div>
-                                    <Choose>
-                                        <When condition={completed_at}>
-                                            <div><time className="date" dateTime="" style={{ color: 'white', fontWeight: 'unset', marginTop: '-50px', fontSize: '17px' }}>
-                                                {`Completed ${moment(completed_at).format("MM/DD/YY")} at ${moment(completed_at).format('hh:mm')}`}
-                                            </time></div>
-                                        </When>
-                                        <Otherwise>
-                                            <div><time className="date" dateTime="" style={{ color: 'white', fontWeight: 'unset', marginTop: '-50px', fontSize: '17px' }}>
-                                                {`Assigned ${assignment_date} at ${assignTime}`}
-                                            </time></div>
-                                            <div><time className="date" dateTime="" style={{ color: 'white', fontWeight: 'unset', marginTop: '-28px', fontSize: '17px' }}>
-                                                {due_date && (`Due ${due_date} at ${dueTime}`)}
-                                            </time></div>
-                                        </Otherwise>
-                                    </Choose>
+                                    </div>
                                 </div>
+                                <Choose>
+                                    <When condition={completed_at}>
+                                        <div><time className="date" dateTime="" style={{ color: 'white', fontWeight: 'unset', marginTop: '-50px', fontSize: '17px' }}>
+                                            {`Completed ${moment(completed_at).format("MM/DD/YY")} at ${moment(completed_at).format('hh:mm')}`}
+                                        </time></div>
+                                    </When>
+                                    <Otherwise>
+                                        <div><time className="date" dateTime="" style={{ color: 'white', fontWeight: 'unset', marginTop: '-50px', fontSize: '17px' }}>
+                                            {`Assigned ${assignment_date} at ${assignTime}`}
+                                        </time></div>
+                                        <div><time className="date" dateTime="" style={{ color: 'white', fontWeight: 'unset', marginTop: '-28px', fontSize: '17px' }}>
+                                            {due_date && (`Due ${due_date} at ${dueTime}`)}
+                                        </time></div>
+                                    </Otherwise>
+                                </Choose>
                             </div>
                         </div>
                         <div className='header-row-block card-panel-row row'>
@@ -129,6 +143,7 @@ ReadWorkBook.propTypes = {
 
 const mapStateToProps = createStructuredSelector({
     lesson: makeSelectActiveLesson(),
+    units: makeSelectUnitFilterOptions(),
 });
 
 const mapDispatchToProps = (dispatch) => {
