3ed62f4e6cf20b4395e04c1db315aef36adcc9ae
unknown <nosracer_gt@sbcglobal.net>
2020-05-06 12:14:43 -0500

    Create Freeresponse component to display non multiple choice answers in answer sheet

---
+++ components/Student/LessonDetailAnswerSheet/components/FreeResponse/index.js
@@ -0,0 +1,109 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const styles = {
+  red: {
+    position: "relative",
+    color: "#fff",
+    borderColor: "#fff",
+    border: "1px solid",
+    backgroundColor: "#db1d41",
+    height: "19px",
+    width: "19px",
+    borderRadius: "50%",
+  },
+  greenFilled: {
+    position: "relative",
+    color: "#fff",
+    borderColor: "#32955c",
+    border: "1px solid",
+    backgroundColor: "#32955c",
+    height: "19px",
+    width: "19px",
+    borderRadius: "50%",
+  },
+  greenBorderOnly: {
+    position: "relative",
+    color: "#32955c",
+    borderColor: "#32955c",
+    border: "1px solid",
+    backgroundColor: "#fff",
+    height: "19px",
+    width: "19px",
+    borderRadius: "50%",
+  },
+  plain: {
+    position: "relative",
+    color: "#a6a8ab",
+    border: "1px solid",
+    borderColor: "#a6a8ab",
+    height: "19px",
+    width: "19px",
+    borderRadius: "50%",
+  },
+};
+
+class FreeResponse extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  mapEmptyBubbles = (id) => {
+    const { lesson: is_correct } = this.props;
+    return (
+      <>
+        <li>
+          <span style={this.renderBubbleStyle()}></span>
+        </li>
+        <li>{!is_correct && <span style={this.renderBubbleStyle()}></span>}</li>
+      </>
+    );
+  };
+
+  getCorrectAnswerIndexMatchedId = (answer) => answer === this.props.lesson.correct_answer_id;
+  getStudentAnswerIndexMatchedId = (answer) => answer === this.props.lesson.answer_id;
+
+  getCorrectAnswerIndex = (answerIds) => {
+    const correctAnswerIndex = answerIds.findIndex(this.getCorrectAnswerIndexMatchedId);
+    return correctAnswerIndex;
+  };
+  getStudentAnswerIndex = (answerIds) => {
+    const studentAnswerIndex = answerIds.findIndex(this.getStudentAnswerIndexMatchedId);
+    return studentAnswerIndex;
+  };
+
+  renderBubbleStyle = (index) => {
+    const {
+      lesson: { is_correct },
+    } = this.props;
+    const answerIds = this.props.lesson.problem.answers.map((answer) => answer.id);
+    const correctAnswerIndex = this.getCorrectAnswerIndex(answerIds);
+    const studentAnswerIndex = this.getStudentAnswerIndex(answerIds);
+    if (correctAnswerIndex === index && is_correct) {
+      return styles.greenFilled;
+    }
+    if (correctAnswerIndex === index && !is_correct) {
+      return styles.greenBorderOnly;
+    }
+    if (index === studentAnswerIndex && !is_correct) {
+      return styles.red;
+    }
+    if (index !== studentAnswerIndex && index !== correctAnswerIndex) {
+      return styles.plain;
+    }
+    return styles.plain;
+  };
+
+  render() {
+    const {
+      lesson: { id },
+    } = this.props;
+    return <React.Fragment>{this.mapEmptyBubbles(id)}</React.Fragment>;
+  }
+}
+
+FreeResponse.propTypes = {
+  lesson: PropTypes.object.isRequired,
+};
+
+export default FreeResponse;
